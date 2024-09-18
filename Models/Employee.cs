0d273b3cc51aea0e0036abcec585dd31611981d6
Matthew Desabelle <desabellematthew@gmail.com>
2024-09-19 01:19:48 +0800

    changes to models, remove namespace wrapping

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,20 +1,19 @@
-namespace ProjectAPI.Models
+namespace ProjectAPI.Models;
+
+using System.Text.Json.Serialization;
+
+/// <summary>
+/// Represents an employee within the system.
+/// </summary>
+public class Employee
 {
-    using System.Text.Json.Serialization;
+    public int EmployeeId { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required int ProjectId { get; set; }
 
-    /// <summary>
-    /// Represents an employee within the system.
-    /// </summary>
-    public class Employee
-    {
-        public int EmployeeId { get; set; }
-        public required string FirstName { get; set; }
-        public required string LastName { get; set; }
-        public required int ProjectId { get; set; }
-
-        /// Gets or sets the project associated with the employee. This property is optional and can be null.
-        /// It is marked with [JsonIgnore] to prevent serialization during JSON operations.
-        [JsonIgnore]
-        public Project? Project { get; set; }
-    }
+    /// Gets or sets the project associated with the employee. This property is optional and can be null.
+    /// It is marked with [JsonIgnore] to prevent serialization during JSON operations.
+    [JsonIgnore]
+    public Project? Project { get; set; }
 }
--- Models/Project.cs
+++ Models/Project.cs
@@ -1,19 +1,19 @@
-namespace ProjectAPI.Models
+namespace ProjectAPI.Models;
+
+/// <summary>
+/// Represents a project within the system.
+/// </summary>
+public class Project
 {
-    /// <summary>
-    /// Represents a project within the system.
-    /// </summary>
-    public class Project
-    {
-        public int ProjectId { get; set; }
-        public required string Name { get; set; }
-        public required string Description { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
-        
-        /// Gets or sets the collection of employees assigned to the project.
-        /// A project can have multiple employees assigned to it.
-        /// This property is optional and can be null.
-        public ICollection<Employee>? Employees { get; set; }
-    }
+    public int ProjectId { get; set; }
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    
+    /// Gets or sets the collection of employees assigned to the project.
+    /// A project can have multiple employees assigned to it.
+    /// This property is optional and can be null.
+    public ICollection<Employee>? Employees { get; set; }
 }
+
